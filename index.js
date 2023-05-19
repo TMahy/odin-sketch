745c135a5ee7cd9b1b9d48c8d5f5fc3522f2a276
Tom Mahy <tommahy11@gmail.com>
2023-05-19 18:30:13 +0100

    Updates to styles and script

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-
-
-
 // Initialising the grid of 16*16 squares
 
 const containerDiv = document.querySelector('.container');
 const clearButton = document.querySelector('.btn-clear');
 const newGridButton = document.querySelector('.btn-newGrid');
-  
+
 let gridSize = 16;
+
+const maxGrid = 100;
+const minGrid = 1;
 
 let squares = addSquares(containerDiv, gridSize);
 
@@ -20,23 +20,24 @@ clearButton.addEventListener('click', () =>{
 //On new grid button click, prompt user for input, update the grid
 newGridButton.addEventListener('click', () =>{
     do{
-        inputSize = parseInt(window.prompt("Please enter a number between 8 and 32"));
-    } while(isNaN(inputSize) || inputSize < 8 || inputSize > 32)
+        inputSize = parseInt(window.prompt("Please enter a number between 1 and 100"));
+    } while(isNaN(inputSize) || inputSize < minGrid || inputSize > maxGrid)
     gridSize = inputSize;
     squares = updateGrid(containerDiv, squares, gridSize);
 })
 
 function addSquares(container, gridSize){
-    container.style.maxWidth = `${gridSize * 20}px`;
-    container.style.minWidth = `${gridSize * 20}px`;
     for(let i = 0; i < gridSize**2; i++){
         const square = document.createElement('div');
         square.classList.add('square');
         container.appendChild(square);
 
+        square.style.width = `${window.getComputedStyle(containerDiv).getPropertyValue('width').match(/\d+/)[0] / gridSize}px`
+        square.style.height = `${window.getComputedStyle(containerDiv).getPropertyValue('height').match(/\d+/)[0] / gridSize}px`
+
         //For each square, on mouse over, fill it in black
         square.addEventListener('mousemove', () =>{
-            square.style.backgroundColor = 'black';
+            square.style.backgroundColor = 'black';            
         })
     }
 
@@ -50,9 +51,6 @@ function updateGrid(container, squares, gridSize){
     })
     //Calls add squares with the gridsize, which returns an array of DOM elements 'squares'
     let newSquares = addSquares(container, gridSize);
+
     return newSquares;
 }
-
-
-
-
